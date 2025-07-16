40f611282cc69c51c8343bc277a3b8de993477e6
DSG-UTTN <daniel.santos@uttn.mx>
2025-07-16 13:29:57 -0500

    Initial backend configuration

---
+++ backend/config/db.js
@@ -0,0 +1,30 @@
+/* db.js maneja la conexión a la base de datos PostgreSQL*/
+
+// Importar dotenv para manejar variables de entorno y pg para la conexión a PostgreSQL
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
+
+// Crear un pool de conexiones a la base de datos PostgreSQL
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+// Probar la conexión a la base de datos
+pool.connect()
+    .then((client) => {
+        console.log('Conexión a la base de datos PostgreSQL exitosa');
+        client.release();
+    })
+    .catch((err) => {
+        console.error('Error al conectar a la base de datos PostgreSQL:', err.message);
+    });
+
+// Exportar el pool para que pueda ser utilizado en otras partes de la aplicación
+module.exports = pool;
